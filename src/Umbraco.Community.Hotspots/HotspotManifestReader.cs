883acc5253785d0faac1729a2f74a83f48d59ac3
Bjarne Fyrstenborg <bf@mcb.dk>
2026-07-13 15:12:07 +0200

    Adjustments

--- src/Umbraco.Community.Hotspots/HotspotManifestReader.cs
+++ src/Umbraco.Community.Hotspots/HotspotManifestReader.cs
@@ -7,35 +7,14 @@ namespace Umbraco.Community.Hotspots
 {
     internal class HotspotManifestReader : IPackageManifestReader
     {
-        /*public void Filter(List<PackageManifest> manifests)
-        {
-            var assembly = typeof(HotspotManifestReader).Assembly;
-
-            manifests.Add(new PackageManifest
-            {
-                PackageName = "Umbraco Community Hotspots",
-                Version = assembly.GetName()?.Version?.ToString(3) ?? "0.1.0",
-                AllowPackageTelemetry = true,
-                Scripts =
-                [
-                    // List any Script files
-                    // Urls should start '/App_Plugins/Umbraco.Community.Hotspots/' not '/wwwroot/Umbraco.Community.Hotspots/', e.g.
-                    "/App_Plugins/Umbraco.Community.Hotspots/hotspot.controller.js",
-                    "/App_Plugins/Umbraco.Community.Hotspots/source-image.controller.js"
-                ],
-                Stylesheets =
-                [
-                    // List any Stylesheet files
-                    // Urls should start '/App_Plugins/Umbraco.Community.Hotspots/' not '/wwwroot/Umbraco.Community.Hotspots/', e.g.
-                    "/App_Plugins/Umbraco.Community.Hotspots/styles.css"
-                ]
-            });
-        }*/
-
         public Task<IEnumerable<PackageManifest>> ReadPackageManifestsAsync()
         {
             var versionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
             var version = $"{versionInfo.FileMajorPart}.{versionInfo.FileMinorPart}.{versionInfo.FileBuildPart}";
+
+            // get info from assembly
+            //Assembly assembly = typeof(UmbracoPackage).Assembly;
+            //var version = assembly.GetName().Version?.ToString() ?? "0.0.0";
 
             var manifest = (IEnumerable<PackageManifest>)
             [
@@ -54,7 +33,6 @@ namespace Umbraco.Community.Hotspots
                             Js = "/App_Plugins/Umbraco.Community.Hotspots/umbraco-community-hotspots.js"
                         }
                     ]
-
                 }
             ];
 
