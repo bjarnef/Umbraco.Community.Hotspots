30d7b9fb53c904882f9906e07eabd29fcdc57d04
Bjarne Fyrstenborg <bf@mcb.dk>
2026-07-26 20:34:54 +0200

    Register notification handler

--- src/Umbraco.Community.Hotspots/Composers/HotspotComposer.cs
+++ src/Umbraco.Community.Hotspots/Composers/HotspotComposer.cs
@@ -1,12 +1,18 @@
 using Microsoft.Extensions.DependencyInjection;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Notifications;
 using Umbraco.Cms.Infrastructure.Manifest;
+using Umbraco.Community.Hotspots.Migrations;
 
 namespace Umbraco.Community.Hotspots.Composers
 {
     internal class HotspotComposer : IComposer
     {
-        public void Compose(IUmbracoBuilder builder) => builder.Services.AddSingleton<IPackageManifestReader, HotspotManifestReader>();
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.Services.AddSingleton<IPackageManifestReader, HotspotManifestReader>();
+            builder.AddNotificationAsyncHandler<UmbracoApplicationStartingNotification, RunMigrationsHandler>();
+        }
     }
 }
