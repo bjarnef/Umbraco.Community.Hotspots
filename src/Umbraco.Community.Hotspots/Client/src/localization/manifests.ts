a129be04a89602d7464eaa66ed3fa5bc61395b0d
Bjarne Fyrstenborg <bf@mcb.dk>
2026-07-26 20:17:47 +0200

    Adjust localization

--- src/Umbraco.Community.Hotspots/Client/src/localization/manifests.ts
+++ src/Umbraco.Community.Hotspots/Client/src/localization/manifests.ts
@@ -3,31 +3,49 @@ import { ManifestLocalization } from "@umbraco-cms/backoffice/localization";
 const localizations: Array<ManifestLocalization> = [
   {
     type: "localization",
-    alias: "Umbraco.Community.Hotspots.Localize.DaDK",
-    name: "Danish (Denmark)",
-    meta: {
-      culture: "da-dk"
-    },
-    js: () => import("./da-dk"),
-  },
-  {
-    type: "localization",
-    alias: "Umbraco.Community.Hotspots.Localize.Da",
-    name: "Danish",
+    alias: "Umbraco.Community.Hotspots.Localization.Da",
+    name: "Hotspot Danish Localization",
     meta: {
       culture: "da"
     },
-    js: () => import("./da-dk"),
+    js: () => import("./da-dk.js"),
   },
   {
     type: "localization",
-    alias: "Umbraco.Community.Hotspots.Localize.EnUs",
-    name: "English (United States)",
+    alias: "Umbraco.Community.Hotspots.Localization.DaDK",
+    name: "Hotspot Danish (Denmark) Localization",
+    meta: {
+      culture: "da-dk"
+    },
+    js: () => import("./da-dk.js"),
+  },
+  {
+    type: "localization",
+    alias: "Umbraco.Community.Hotspots.Localization.EnUs",
+    name: "Hotspot English (United States) Localization",
     meta: {
       culture: "en-us"
     },
-    js: () => import("./en-us"),
+    js: () => import("./en-us.js"),
   },
+  {
+    type: 'localization',
+    alias: 'Umbraco.Community.Hotspots.Localization.NbNo',
+    name: 'Hotspot Norwegian (Norway) Localization',
+    meta: {
+      culture: 'nb-no'
+    },
+    js: () => import('./nb-no.js'),
+  },
+  {
+    type: 'localization',
+    alias: 'Umbraco.Community.Hotspots.Localization.SvSe',
+    name: 'Hotspot Swedish (Sweden) Localization',
+    meta: {
+      culture: 'sv-se'
+    },
+    js: () => import('./sv-se.js'),
+  }
 ];
 
 export const manifests = localizations;
